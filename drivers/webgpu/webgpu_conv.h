245c1fcba48e7512bb80b066270e6c1951a9aa83
dav <usr21p1@gmail.com>
2024-04-08 11:03:53 -0700

    Implement sampler create and free

--- drivers/webgpu/webgpu_conv.h
+++ drivers/webgpu/webgpu_conv.h
@@ -6,5 +6,9 @@
 
 WGPUBufferUsage webgpu_buffer_usage_from_rd(BitField<RDD::BufferUsageBits> p_buffer_usage);
 WGPUTextureFormat webgpu_texture_format_from_rd(RDD::DataFormat p_data_format);
+WGPUFilterMode webgpu_filter_mode_from_rd(RDD::SamplerFilter p_sampler_filter);
+WGPUMipmapFilterMode webgpu_mipmap_filter_mode_from_rd(RDD::SamplerFilter p_sampler_filter);
+WGPUAddressMode webgpu_address_mode_from_rd(RDD::SamplerRepeatMode p_sampler_repeat_mode);
+WGPUCompareFunction webgpu_compare_mode_from_rd(RDD::CompareOperator p_compare_operator);
 
 #endif // WEBGPU_CONV_H
