e07227c2ba869b44ced08bb0608673394fe0f7fa
dav <usr21p1@gmail.com>
2024-04-08 10:22:51 -0700

    ixes and stubs up to first sampler_create

--- drivers/webgpu/webgpu_conv.h
+++ drivers/webgpu/webgpu_conv.h
@@ -4,6 +4,7 @@
 #include "servers/rendering/rendering_device.h"
 #include <webgpu.h>
 
-WGPUBufferUsage rd_to_webgpu_buffer_usage(BitField<RDD::BufferUsageBits> p_buffer_usage);
+WGPUBufferUsage webgpu_buffer_usage_from_rd(BitField<RDD::BufferUsageBits> p_buffer_usage);
+WGPUTextureFormat webgpu_texture_format_from_rd(RDD::DataFormat p_data_format);
 
 #endif // WEBGPU_CONV_H
